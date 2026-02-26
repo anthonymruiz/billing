029735022fa21d97b4d44793302aa0e4df28c8c1
Anthony <anthony.moronta@triplcyber.com.do>
2026-02-26 10:44:10 -0400

    changes

--- src/app/pages/home/home.routes.ts
+++ src/app/pages/home/home.routes.ts
@@ -1,14 +1,11 @@
 import { Routes } from '@angular/router';
-import { Home } from './home';     
-import { Cancel } from '../cancel/cancel';
+import { Home } from './home';      
 import { Cancelled } from '../cancelled/cancelled';
-import { Active } from '../active/active';
-import { OrderSummary } from '../order-summary/order-summary';
+import { Active } from '../active/active'; 
 import { Expired } from '../expired/expired';
 import { SessionClosed } from '../session-closed/session-closed';
 import { LogoutComponent } from '../../core/components/logout/logout.component';
-import { authGuard } from '../../core/guards/guard';
-import { ConfirmCancelation } from '../confirm-cancelation/confirm-cancelation';
+import { authGuard } from '../../core/guards/guard'; 
 import { Reactive } from '../reactive/reactive';
 import { Success } from '../success/success';
 
@@ -39,26 +36,26 @@ export const HOME_ROUTES: Routes = [
     component: Active,
     canActivate: [authGuard]
   },
-  { 
-    path: 'confirm', 
-    component: ConfirmCancelation,
-    canActivate: [authGuard]
-  },  
-  { 
-    path: 'cancel', 
-    component: Cancel,
-    canActivate: [authGuard]
-  }, 
+  // { 
+  //   path: 'confirm', 
+  //   component: ConfirmCancelation,
+  //   canActivate: [authGuard]
+  // },  
+  // { 
+  //   path: 'cancel', 
+  //   component: Cancel,
+  //   canActivate: [authGuard]
+  // }, 
   { 
     path: 'cancelled', 
     component: Cancelled,
     canActivate: [authGuard]
   }, 
-  { 
-    path: 'summary', 
-    component: OrderSummary,
-    canActivate: [authGuard]
-  },  
+  // { 
+  //   path: 'summary', 
+  //   component: OrderSummary,
+  //   canActivate: [authGuard]
+  // },  
   { 
     path: 'expired', 
     component: Expired,
